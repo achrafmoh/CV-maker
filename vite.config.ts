289b616e08c289d223a338ff90c48db449d1ea44
achrafmoh <131202784+achrafmoh@users.noreply.github.com>
2025-09-29 13:21:45 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,12 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
-    plugins: [react()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      }
-    },
-    build: {
-      outDir: 'dist',
-      sourcemap: false
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env.GEMINI_API_KEY': JSON.stringify(process.env.GEMINI_API_KEY)
+  },
+  build: {
+    outDir: 'dist'
+  }
 });
